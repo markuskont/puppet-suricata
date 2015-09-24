865a537fd044b481ed7569f380bdd3baa360c252
Paul Gomersbach <p.gomersbach@rely.nl>
2015-09-24 18:07:51 +0200

    install scirius

--- manifests/scirius/install.pp
+++ manifests/scirius/install.pp
@@ -10,22 +10,26 @@ class suricata::scirius::install {
     'python-textile',
     'python-docutils',
     'python-pip',
-    'python-dev'
+    'python-dev',
+    'python-pyinotify'
   ]
   ensure_packages($packages)
-
-
-
-#  # create etpro rules dir
-#  file{ 'etprodir':
-#    ensure => directory,
-#    path   => '/etc/suricata/rules/etpro',
-#  }
-  # create emerging rules dir
-#  file{ 'emergingdir':
-#    ensure => directory,
-#    path   => '/etc/suricata/rules/emerging',
-#  }
-
+  # clone scirius repo
+  vcsrepo { '/opt/scirius':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/StamusNetworks/scirius.git',
+  } ~>
+  # install requierements
+  exec { 'install requirements':
+    command     => '/usr/bin/pip install -r /opt/scirius/requirements.txt',
+    refreshonly => true,
+  }
+  # install new db if not exist
+  exec { 'initial_syncdb':
+    command => '/usr/bin/python manage.py syncdb --no-initial-data --noinput',
+    creates => '/opt/scirius/db.sqlite3',
+    cwd     => '/opt/scirius',
+  }
 }
 
