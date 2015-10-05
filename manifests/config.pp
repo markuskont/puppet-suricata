700550414d687d74cbcfe26755ffe6e2dfe3bc27
Paul Gomersbach <p.gomersbach@rely.nl>
2015-10-05 20:34:15 +0200

    add rules dir

--- manifests/config.pp
+++ manifests/config.pp
@@ -48,20 +48,17 @@ class suricata::config {
   file{ 'logdir':
     ensure => directory,
     path   => '/var/log/suricata',
-  } ~>
+  }
+# create rules dir
+  file{ 'rulesdir':
+    ensure => directory,
+    path   => '/etc/suricata/rules',
+  } 
 # create suricata configs
-  file{ 'classification.config':
-    path    => '/etc/suricata/classification.config',
-    content => template('suricata/classification.config.erb'),
-  } ~>
-  file{ 'reference.config':
-    path    => '/etc/suricata/reference.config',
-    content => template('suricata/reference.config.erb'),
-  } ~>
   file{ 'suricata-default':
     path    => '/etc/default/suricata',
     content => template('suricata/suricata-default.erb'),
-  } ~>
+  }
   file{ 'suricata.yaml':
     path    => '/etc/suricata/suricata.yaml',
     content => template('suricata/suricata.yaml.erb'),
