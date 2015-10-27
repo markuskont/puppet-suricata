0ba25e67c7a5a8c6061d51dcc46cebc5b92d3697
Paul Gomersbach <p.gomersbach@gmail.com>
2015-10-27 12:52:19 +0100

    add HOME_NET parameter

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,6 +5,7 @@
 #
 class suricata::params {
   $ruleset = 'emerging'
+  $home_net = '192.168.0.0/16,10.0.0.0/8,172.16.0.0/12'
   $threads = $::processorcount
   $scirius_admin = 'admin'
   $scirius_admin_pass = 'changeme'
