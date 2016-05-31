261f3ee0e341058f0bca57ec82b820e1cb1f0e69
Markus <markus@spin.ee>
2016-05-31 16:48:32 +0300

    Fact which can easily be used for configuring multiple listeners. Ignore loopback, primary (or NAT) interface, vpn tunnels and virtual interfaces.

---
+++ lib/facter/secondary_interfaces.rb
@@ -0,0 +1,13 @@
+Facter.add(:secondary_interfaces) do
+  confine :kernel => 'Linux'
+  networking = Facter.value(:networking)
+  secondary_interfaces = []
+  networking['interfaces'].each do |interface,details|
+    if (interface !~ /lo/ && interface != networking['primary'] && interface !~ /(?:tun|tap)\d+/ && interface !~ /.+:.+/)
+      secondary_interfaces << interface
+    end
+  end
+  setcode do
+    secondary_interfaces
+  end
+end
