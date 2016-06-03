7ee11ca0678d19e244b0b27570fdb46c09d5d9ba
Markus <markus@spin.ee>
2016-06-03 13:28:18 +0300

    Script was unable to understad if interface was up or down. Thus, listener was created for disabled interfaces. Causing suricata crash

--- lib/facter/secondary_interfaces.rb
+++ lib/facter/secondary_interfaces.rb
@@ -4,7 +4,7 @@ Facter.add(:secondary_interfaces) do
   secondary_interfaces = []
   networking['interfaces'].each do |interface,details|
     if (interface !~ /lo/ && interface != networking['primary'] && interface !~ /(?:tun|tap)\d+/ && interface !~ /.+:.+/)
-      secondary_interfaces << interface
+      secondary_interfaces << interface unless details['bindings'].nil?
     end
   end
   setcode do
