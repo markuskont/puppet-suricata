de194029c08eb02b83581709dacce08dac60be19
Markus <markus@spin.ee>
2016-06-07 13:32:34 +0300

    Checksumming configuration only applied to single IF

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,43 +8,80 @@ class suricata::config inherits suricata {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  # disable offloading
-  # generic-receive-offload
-  exec { 'disable_gro':
-    command => "/sbin/ethtool -K ${monitor_interface} gro off",
-    unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-receive-offload: (off|on \[fixed\])'",
-  }
-  # rx-vlan-offload
-  exec { 'disable_rxvlan':
-    command => "/sbin/ethtool -K ${monitor_interface} rxvlan off",
-    unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-vlan-offload: (off|on \[fixed\])'",
-  }
-  # generic-segmentation-offload
-  exec { 'disable_gso':
-    command => "/sbin/ethtool -K ${monitor_interface} gso off",
-    unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-segmentation-offload: (off|on \[fixed\])'",
-  }
-  # tcp-segmentation-offload
-  exec { 'disable_sg':
-    command => "/sbin/ethtool -K ${monitor_interface} sg off",
-    unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'tcp-segmentation-offload: (off|on \[fixed\])'",
-  }
-  # rx-checksumming
-  exec { 'disable_rx':
-    command => "/sbin/ethtool -K ${monitor_interface} rx off",
-    unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-checksumming: (off|on \[fixed\])'",
-  }
-
-  # set promisc mode
-  exec { 'set_promisc':
-    command => "/sbin/ifconfig ${monitor_interface} promisc",
-    unless  => "/sbin/ifconfig ${monitor_interface} | grep 'PROMISC'",
-  }
-
-  # enable interface
-  exec { 'set_enable':
-    command => "/sbin/ifconfig ${monitor_interface} up",
-    unless  => "/sbin/ifconfig ${monitor_interface} | grep 'UP'",
+  if $suricata::monitor_interface_array {
+    $suricata::monitor_interface_array.each |String $monitor_interface| {
+      exec { "${monitor_interface}_disable_gro":
+        command => "/sbin/ethtool -K ${monitor_interface} gro off",
+        unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-receive-offload: (off|on \[fixed\])'",
+      }
+      # rx-vlan-offload
+      exec { "${monitor_interface}_disable_rxvlan":
+        command => "/sbin/ethtool -K ${monitor_interface} rxvlan off",
+        unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-vlan-offload: (off|on \[fixed\])'",
+      }
+      # generic-segmentation-offload
+      exec { "${monitor_interface}_disable_gso":
+        command => "/sbin/ethtool -K ${monitor_interface} gso off",
+        unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-segmentation-offload: (off|on \[fixed\])'",
+      }
+      # tcp-segmentation-offload
+      exec { "${monitor_interface}_disable_sg":
+        command => "/sbin/ethtool -K ${monitor_interface} sg off",
+        unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'tcp-segmentation-offload: (off|on \[fixed\])'",
+      }
+      # rx-checksumming
+      exec { "${monitor_interface}_disable_rx":
+        command => "/sbin/ethtool -K ${monitor_interface} rx off",
+        unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-checksumming: (off|on \[fixed\])'",
+      }
+      # set promisc mode
+      exec { "${monitor_interface}_set_promisc":
+        command => "/sbin/ifconfig ${monitor_interface} promisc",
+        unless  => "/sbin/ifconfig ${monitor_interface} | grep 'PROMISC'",
+      }
+      # enable interface
+      exec { "${monitor_interface}_set_enable":
+        command => "/sbin/ifconfig ${monitor_interface} up",
+        unless  => "/sbin/ifconfig ${monitor_interface} | grep 'UP'",
+      }
+    }
+  } else {
+    # disable offloading
+    # generic-receive-offload
+    exec { "${monitor_interface}_disable_gro":
+      command => "/sbin/ethtool -K ${monitor_interface} gro off",
+      unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-receive-offload: (off|on \[fixed\])'",
+    }
+    # rx-vlan-offload
+    exec { "${monitor_interface}_disable_rxvlan":
+      command => "/sbin/ethtool -K ${monitor_interface} rxvlan off",
+      unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-vlan-offload: (off|on \[fixed\])'",
+    }
+    # generic-segmentation-offload
+    exec { "${monitor_interface}_disable_gso":
+      command => "/sbin/ethtool -K ${monitor_interface} gso off",
+      unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'generic-segmentation-offload: (off|on \[fixed\])'",
+    }
+    # tcp-segmentation-offload
+    exec { "${monitor_interface}_disable_sg":
+      command => "/sbin/ethtool -K ${monitor_interface} sg off",
+      unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'tcp-segmentation-offload: (off|on \[fixed\])'",
+    }
+    # rx-checksumming
+    exec { "${monitor_interface}_disable_rx":
+      command => "/sbin/ethtool -K ${monitor_interface} rx off",
+      unless  => "/sbin/ethtool -k ${monitor_interface} | egrep 'rx-checksumming: (off|on \[fixed\])'",
+    }
+    # set promisc mode
+    exec { "${monitor_interface}_set_promisc":
+      command => "/sbin/ifconfig ${monitor_interface} promisc",
+      unless  => "/sbin/ifconfig ${monitor_interface} | grep 'PROMISC'",
+    }
+    # enable interface
+    exec { "${monitor_interface}_set_enable":
+      command => "/sbin/ifconfig ${monitor_interface} up",
+      unless  => "/sbin/ifconfig ${monitor_interface} | grep 'UP'",
+    }
   }
 
   # create logdir
